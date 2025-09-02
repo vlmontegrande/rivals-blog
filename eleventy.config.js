0ee9952d7fc7eafeee96543e365198537e10bb2f
vlmontegrande <vlmontegrande@gmail.com>
2025-09-01 20:09:43 -0700

    Add css file to _site directory

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,14 @@
+import markdownIt from "markdown-it";
+
 export default async function(eleventyConfig) {
   eleventyConfig.setFrontMatterParsingOptions({
 		language: "json"
 	});
+  let options = {
+		html: true
+	};
+  eleventyConfig.addPassthroughCopy("css/*.css");
+  eleventyConfig.setLibrary("md", markdownIt(options));
 };
 
 export const config = {
